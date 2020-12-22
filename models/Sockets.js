c569da92d7c395e9427a7d3ee8ed105f5a67fa72
Rodrigo García Ávila <rodrigogarciaavila26@gmail.com>
2020-12-22 16:47:53 -0600

    Connect and disconnect socket

--- models/Sockets.js
+++ models/Sockets.js
@@ -6,7 +6,8 @@ class Sockets{
 
     socketEvents(){
         // On Connection Where 'socket' is the client connected
-        this.io.on('connection', (socket) => {                 
+        this.io.on('connection', (socket) => {     
+            console.log('client connected!!');            
             // Validate JWT
             // If token is no valid, disconnect
             // What user active is ?
@@ -14,7 +15,9 @@ class Sockets{
             // Socket Join by uid
             // Hear when a user sends a message
             // Disconnect user an set user off on DBB   
-                    
+            socket.on('disconnect',( ) => { 
+
+             });
          });
     }
 }
